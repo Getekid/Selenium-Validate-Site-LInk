c90a95ce588ebd896993d80eb556a00e10c87d85
Thomas Pappas <getekid@gmail.com>
2019-03-31 20:17:05 +0300

    Converted script to a class

--- selenium-validate-site-links.py
+++ selenium-validate-site-links.py
@@ -1,21 +1,31 @@
 from selenium import webdriver
 import time
 
-protocol = 'https://'
-domain = 'www.w3.org'
+
+class SiteAllLinkValidator:
+
+    # Initiate the class with the domain and protocol variables
+    def __init__(self, driver, domain, protocol='https://'):
+        self.driver = driver
+        self.domain = domain
+        self.protocol = protocol
+
+    def validate_link(self):
+        self.driver.get(self.protocol + self.domain)
+        # Iterate through all the a tags of the page.
+        for a in self.driver.find_elements_by_xpath('.//a'):
+            uri = a.get_attribute('href')
+
+            # Check whether the uri is an internal domain,
+            # either relative or starts with the domain.
+            if uri.startswith('http://') or uri.startswith('http://'):
+                if not uri.startswith('https://' + self.domain) and not uri.startswith('http://' + self.domain):
+                    continue
+            print(uri)
+
 
 driver = webdriver.Chrome()
-driver.get(protocol + domain)
-# Iterate through all the a tags of the page.
-for a in driver.find_elements_by_xpath('.//a'):
-    uri = a.get_attribute('href')
-
-    # Check whether the uri is an internal domain,
-    # either relative or starts with the domain.
-    if uri.startswith('http://') or uri.startswith('http://'):
-        if not uri.startswith('https://' + domain) and not uri.startswith('http://' + domain):
-            continue
-    print(uri)
-
+validator = SiteAllLinkValidator(driver, 'www.w3.org')
+validator.validate_link()
 time.sleep(3)
 driver.quit()
