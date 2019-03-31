d936612d295d0ffcd1d70f0590549f9fa6dbd41a
Thomas Pappas <getekid@gmail.com>
2019-03-31 23:46:07 +0300

    Added time_to_stop class variable and visited_links links to skip same links

--- selenium-validate-site-links.py
+++ selenium-validate-site-links.py
@@ -5,19 +5,23 @@ from urllib.parse import urljoin
 
 class SiteAllLinkValidator:
 
-    def __init__(self, driver, domain, error_page_title, protocol='https://'):
+    def __init__(self, driver, domain, error_page_title, time_to_wait=0, protocol='https://'):
         """Initiate the class with the following variables
 
         Args:
             driver: The selenium webdriver object.
             domain (str): The domain of the website.
             error_page_title (str): The Title of the error page.
+            time_to_wait (int): Number of seconds to wait after each link visit.
             protocol (str): The protocol to be used, defaults in 'https://'.
         """
         self.driver = driver
         self.domain = domain
         self.error_page_title = error_page_title
         self.protocol = protocol
+        self.time_to_wait = time_to_wait
+        # Initiate a list to store the visited links.
+        self.visited_links = []
 
     def validate_link(self, link):
         """Validate a link recursively.
@@ -34,15 +38,21 @@ class SiteAllLinkValidator:
         if not self.is_internal(link):
             return ''
 
-        self.driver.get(urljoin(self.driver.current_url, link))
+        url = urljoin(self.driver.current_url, link)
+        # Skip if URL has been visited already.
+        if url in self.visited_links:
+            return ''
+
+        # Go to the URL and validate it.
+        self.driver.get(url)
+        time.sleep(self.time_to_wait)
         assert self.driver.title != self.error_page_title
+        self.visited_links.append(url)
 
         # Iterate through all the a tags of the page.
         for a in self.driver.find_elements_by_xpath('.//a'):
             link = a.get_attribute('href')
-            # TODO: Add condition to end the loop.
-            # self.validate_link(link)
-            print(link)
+            self.validate_link(link)
 
     def is_internal(self, uri):
         """Checks whether the uri is an internal domain,
@@ -54,6 +64,8 @@ class SiteAllLinkValidator:
         Returns:
             bool: Whether the URI is internal or not.
         """
+        if not isinstance(uri, str):
+            return False
         if uri.startswith('https://') or uri.startswith('http://'):
             if not uri.startswith('https://' + self.domain) and not uri.startswith('http://' + self.domain):
                 return False
@@ -61,7 +73,6 @@ class SiteAllLinkValidator:
 
 
 driver = webdriver.Chrome()
-validator = SiteAllLinkValidator(driver, 'www.w3.org', '404 error page')
+validator = SiteAllLinkValidator(driver, 'www.w3.org', '404 error page', 2)
 validator.validate_link('https://www.w3.org')
-time.sleep(3)
 driver.quit()
