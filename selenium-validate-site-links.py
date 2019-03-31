5ff4cd261ddb4274fcfccf56eeb74384310b6508
Thomas Pappas <getekid@gmail.com>
2019-03-31 18:51:53 +0300

    Included check for internal paths only

--- selenium-validate-site-links.py
+++ selenium-validate-site-links.py
@@ -1,8 +1,18 @@
 from selenium import webdriver
 import time
 
+protocol = 'https://'
+domain = 'www.w3.org'
+
 driver = webdriver.Chrome()
-driver.get('https://www.w3.org/')
+driver.get(protocol + domain)
+# Iterate through all the a tags of the page.
 for a in driver.find_elements_by_xpath('.//a'):
-    print(a.get_attribute('href'))
-    
+    uri = a.get_attribute('href')
+
+    # Check whether the uri is an internal domain,
+    # either relative or starts with the domain.
+    if uri.startswith('http://') or uri.startswith('http://'):
+        if not uri.startswith('https://' + domain) and not uri.startswith('http://' + domain):
+            continue
+    print(uri)
